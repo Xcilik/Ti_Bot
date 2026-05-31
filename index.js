02e19fd9f0751eb062fa2fd8fadaa30b43e1fba2
Xcilik <149920327+Xcilik@users.noreply.github.com>
2026-05-31 21:14:01 +0700

    Update index.js

--- index.js
+++ index.js
@@ -45,12 +45,12 @@ const userInfoSyt = () => {
 	}
 }
 
-try {
-	dns.setServers(['8.8.8.8', '1.1.1.1']);
-	console.log(chalk.yellowBright('[SYSTEM] Custom DNS Google & Cloudflare.'));
-} catch (e) {
-	console.log(chalk.yellowBright('[SYSTEM] failed to custom DNS:'), e.message);
-}
+// try {
+// 	dns.setServers(['8.8.8.8', '1.1.1.1']);
+// 	console.log(chalk.yellowBright('[SYSTEM] Custom DNS Google & Cloudflare.'));
+// } catch (e) {
+// 	console.log(chalk.yellowBright('[SYSTEM] failed to custom DNS:'), e.message);
+// }
 
 // Fetch Api
 global.fetchApi = async (endpoint = '/', data = {}, options = {}) => {
